4fd66d4cc628d5136ab15311c0e09b52bc58210f
denizedaza <dazadenize@gmail.com>
2022-07-22 17:23:28 -0400

    change incorrectly pointing variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { Student } from "./types/student";
@@ -7,17 +7,22 @@ import DataSearch from "./components/dataSearch";
 
 function App() {
   const [students, setStudents] = useState<Array<Student>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   async function fetchData() {
+    setLoading(true);
     try {
-      const result = await axios.get(
-        "https://api.hatchways.io/assessment/students"
-      );
-      const data = result.data.students;
-      data.forEach((student: { tags: string[]; }) => {
-        student.tags = [];
-      })
-      setStudents(data);
+      await axios
+        .get("https://api.hatchways.io/assessment/students")
+        .then((result) => {
+          const data = result.data.students;
+          data.forEach((student: { tags: string[] }) => {
+            student.tags = [];
+          });
+          setStudents(data);
+          setLoading(false);
+          // console.log(students);
+        });
     } catch (error) {
       console.log(error);
     }
@@ -26,15 +31,15 @@ function App() {
   useEffect(() => {
     fetchData();
   }, []);
-  
+
   const [filteredNameSearch, setFilteredNameSearch] =
     useState<Array<Student>>(students);
   const [filteredTagSearch, setFilteredTagSearch] =
     useState<Array<Student>>(students);
 
   const addStudentTag = (tag: string, index: number) => {
-    const tempStudent = [...students]
-    tempStudent[index].tags.push(tag)
+    const tempStudent = [...students];
+    tempStudent[index].tags.push(tag);
     setStudents(tempStudent);
   };
 
@@ -55,55 +60,64 @@ function App() {
   };
 
   const handleTagSearch = (tagValue: string) => {
-    const tagsSearch = tagValue
+    const tagsSearch = tagValue;
     if (tagsSearch !== "") {
       let filteredTagsStudents: Array<Student> = [];
-      students.forEach(student => {
+      students.forEach((student) => {
         let tagPresent = false;
-        student.tags.forEach(tag => {
+        student.tags.forEach((tag) => {
           if (tag.toLowerCase().includes(tagsSearch)) {
             tagPresent = true;
           }
-        })
+        });
 
         if (!tagsSearch || tagPresent) {
-          filteredTagsStudents.push(student)
+          filteredTagsStudents.push(student);
         }
-      })
-      console.log(filteredTagsStudents);
+      });
       setFilteredTagSearch(filteredTagsStudents);
+      // console.log(filteredTagSearch);
     } else {
       setFilteredTagSearch(students);
     }
-  }
+  };
 
-  // console.log(filteredTagSearch)
-  const combinedSearchResults = filteredNameSearch.filter(student => filteredTagSearch.includes(student))
+  // console.log(filteredTagSearch);
+  const mergedSearchResults = [...filteredNameSearch, ...filteredTagSearch];
+  const combinedSearchResults = [
+    ...new Set(mergedSearchResults.map(({ id }) => id)),
+  ].map((el) => students.find(({ id }) => id == el));
 
-  // console.log(combinedSearchResults)
+  // filteredNameSearch.filter((student) =>
+  //   filteredTagSearch.includes(student)
+  // );
+
+  console.log(combinedSearchResults);
 
   let shownStudents: Array<Student> = [];
   filteredNameSearch.length > 0
-    ? (shownStudents = filteredNameSearch)
+    ? (shownStudents = combinedSearchResults)
     : (shownStudents = students);
 
   return (
     <>
-      <div className="outer-box">
-        <div className="search-box-container">
-          <DataSearch handleSearch={handleNameSearch} type="name" />
-          <DataSearch handleSearch={handleTagSearch} type="tag" />
+      {!loading && (
+        <div className="outer-box">
+          <div className="search-box-container">
+            <DataSearch handleSearch={handleNameSearch} type="name" />
+            <DataSearch handleSearch={handleTagSearch} type="tag" />
+          </div>
+          {shownStudents.map((student, index) => (
+            <StudentCard
+              key={index}
+              student={student}
+              index={index}
+              addStudentTag={addStudentTag}
+              tagList={student.tags}
+            />
+          ))}
         </div>
-        {filteredNameSearch.map((student, index) => (
-          <StudentCard
-            key={index}
-            student={student}
-            index={index}
-            addStudentTag={addStudentTag}
-            tagList={student.tags}
-          />
-        ))}
-      </div>
+      )}
     </>
   );
 }
